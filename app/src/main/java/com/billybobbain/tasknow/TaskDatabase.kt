a6906c7c459feb2ba67a8f598153a995f8255833
Wayne Young <billybobbain@gmail.com>
2025-11-15 20:16:02 -0600

    subtasks per task

--- app/src/main/java/com/billybobbain/tasknow/TaskDatabase.kt
+++ app/src/main/java/com/billybobbain/tasknow/TaskDatabase.kt
@@ -6,11 +6,12 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Task::class, Settings::class, Location::class], version = 3, exportSchema = false)
+@Database(entities = [Task::class, Settings::class, Location::class, Subtask::class], version = 4, exportSchema = false)
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
     abstract fun settingsDao(): SettingsDao
     abstract fun locationDao(): LocationDao
+    abstract fun subtaskDao(): SubtaskDao
 
     companion object {
         @Volatile
